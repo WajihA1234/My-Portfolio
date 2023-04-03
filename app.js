5e98511acecb41fa89462a6062333f659f5d40ab
Wajih Aziz <wajihaziz@Wajihs-MacBook-Pro.local>
2023-04-03 19:43:03 -0400

    first commmit

---
+++ app.js
@@ -0,0 +1,13 @@
+ const social = document.getElementById('LI');
+ const otherSocial = document.getElementById('GH');
+
+ social.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/wajih-aziz-182805264/')
+ });
+ 
+//  const otherSocial = document.getElementById('GH');
+// both buttons work but when i put the second const here githubs button doest work
+
+ otherSocial.addEventListener('click', function() {
+    window.open('https://github.com/WajihA1234')
+ });
